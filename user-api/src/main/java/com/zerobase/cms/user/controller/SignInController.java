90d6ae8013428d34134f542dae4f0dcb1c95aae6
JeongwooLee <wjddn3751@gmail.com>
2023-11-30 02:49:45 +0900

    feat : [004-LOGIN_CUSTOMER] Customer에 대한 로그인 구현
    JWT의 내부 암호화
    /customer/*에 대한 token filter ㅈ거용

--- user-api/src/main/java/com/zerobase/cms/user/controller/SignInController.java
+++ user-api/src/main/java/com/zerobase/cms/user/controller/SignInController.java
@@ -1,9 +1,23 @@
 package com.zerobase.cms.user.controller;
 
+import com.zerobase.cms.user.application.SingInApplication;
+import com.zerobase.cms.user.domain.SignInForm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/signin")
+@RequiredArgsConstructor
 public class SignInController {
+
+    private final SingInApplication singInApplication;
+
+    @PostMapping("/customer")
+    public ResponseEntity<String> signInCustomer(@RequestBody SignInForm form) {
+        return ResponseEntity.ok(singInApplication.customerLoginToken(form));
+    }
 }
